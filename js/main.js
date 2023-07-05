005fcbfb281c75f1c280163b6f5923b80a4a8937
aaruanbu <aaruanbu001@gmail.com>
2023-07-05 21:21:27 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,30 @@
+	function btns(){
+	let objec=[{list:"1"},
+		{list:"2"},
+		{list:"3"},
+			{list:"4"},
+				{list:"5"}];
+	
+	let uolis=document.getElementById("unlist")
+	
+	let unordli=objec.map((listes)=>{
+		let listed=document.createElement("li")
+			uolis.append(listed)
+				listed.innerHTML=listes.list;
+	})
+	}
+	// let btn=document.createElement("div")
+	
+	  // btn.setAttribute("class","btn")
+	  // btn.innerHTML="click";
+	  let buto=document.getElementsByClassName("btn");
+		let click=
+			
+		
+	  
+	buto.addEventListener((click,btns())
+	   
+	)
+			
+	
+	
